19f470814a76e1d3b0ba498302dee78ba8400c63
b-reyes <reyesb123@gmail.com>
2025-03-06 19:59:43 -0700

    comment main.py function calls

--- SAM2_Tracking/main.py
+++ SAM2_Tracking/main.py
@@ -8,6 +8,8 @@ yaml_file_path = "./template_configs.yaml"
 # Set device for PyTorch 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# Initialize class using configurations 
 segmenter = SAM2FishSegmenter(configs=yaml_file_path, device=device)
 
+# Run mask propagation workflow 
 segmenter.run_propagation()
